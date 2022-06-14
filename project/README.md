4531d33cc5ccc9167121f87d1258620f8fa0c207
jomin.mathew <Jomin.Mathew@partner.bmw.de>
2022-06-15 00:04:08 +0300

    test class updation

---
+++ project/README.md
@@ -0,0 +1,42 @@
+# Online-TODO-List
+`A simple online TODO list supporting back-end .`
+
+`The application storing necessary data in H2 Database.`
+
+`Built with java 8 and spring boot framework. `
+
+`The functionality  should be demonstrable through API testing (postman)`
+
+
+
+##Resources and samples
+
+1.**local port :** 9031
+
+2.**H2 database console url:** [h2-console url](http://localhost:9031/api/todoItems/h2-console)
+
+3.postman collection name     : To-Do Echo.postman_collection
+
+4.postman collection location : projs\Online-TODO-List
+
+5.input json sample
+    `{
+    "task": "Workout Daily",
+    "done": false
+    }`
+
+
+###Execution steps:
+
+1.Navigate to project folder
+
+2.Execute mvn clean install
+
+3.Execute java -jar D:\projs\Online-TODO-List\project\todo\target\todo-0.0.1-SNAPSHOT.jar
+
+4.Application should be up and running
+
+5.import To-Do Echo.postman_collection to the postman
+
+6.trigger get/save/update/delete functionalities
+
---
+++ project/todo/src/test/java/com/ey/todo/web/TodoControllerTest.java
@@ -0,0 +1,73 @@
+package com.ey.todo.web;
+
+import com.ey.todo.TodoApplication;
+import com.ey.todo.constants.ToDoConstants;
+import com.ey.todo.dto.TodoItem;
+import com.ey.todo.repository.TodoRepository;
+import com.ey.todo.service.TodoService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TodoApplication.class)
+class TodoControllerTest {
+    @InjectMocks
+    private TodoController todoController;
+
+    @Mock
+    private TodoService service;
+    @Mock
+    TodoRepository todoRepository;
+
+    @Test
+    void fetchAllTodoItems() {
+        List<TodoItem> list = new ArrayList<>();
+        TodoItem todoItem = new TodoItem();
+        todoItem.setTask("task1");
+        list.add(todoItem);
+        when(service.fetchAllTodoItems()).thenReturn(list);
+        List<TodoItem> ret = todoController.fetchAllTodoItems();
+        Assertions.assertThat(ret).isEqualTo(list);
+    }
+
+    @Test
+    void updateTodoItems() {
+        TodoItem todoItem = new TodoItem();
+        todoItem.setTask("");
+        when(service.updateTodoItems(any(),any(TodoItem.class))).thenReturn(todoItem);
+        ResponseEntity<?> responseEntity  = todoController.updateTodoItems(1,todoItem);
+        Assertions.assertThat(responseEntity.getBody()).isEqualTo(todoItem);
+    }
+
+    @Test
+    void createNewTodoItems() {
+        TodoItem todoItem = new TodoItem();
+        todoItem.setTask("");
+        when(service.createNewTodoItems(any(TodoItem.class))).thenReturn(ToDoConstants.SUCCESS.toString());
+        String status =todoController.createNewTodoItems(todoItem);
+        Assertions.assertThat(status).isEqualTo(ToDoConstants.SUCCESS.toString());
+
+        when(service.createNewTodoItems(any(TodoItem.class))).thenReturn(ToDoConstants.FAILURE.toString());
+        status =todoController.createNewTodoItems(todoItem);
+        Assertions.assertThat(status).isEqualTo(ToDoConstants.FAILURE.toString());
+    }
+
+    @Test
+    void deleteTodoItem() {
+        when(service.deleteTodoItem(any())).thenReturn(ToDoConstants.DELETED.toString());
+        String ret = todoController.deleteTodoItem(1);
+        Assertions.assertThat(ret).isEqualTo(ToDoConstants.DELETED.toString());
+    }
+}
